f9c002fab44e7965c5c238850f84a4128da1cbe2
Nurgle <axilles90@gmail.com>
2018-11-28 21:49:47 +0300

    Прошли арифметику python и пару фич с sep и and ,а так же /t

---
+++ sep and end.py
@@ -0,0 +1,23 @@
+# инициализация переменной
+
+user = input(" My name is Python, i am language program. What is your name? : ")
+
+# Ответносе сообщение
+
+print( " I'm glad to see you, Welcome")
+
+lang = input(" I cool programming language?:")
+
+
+print (lang, "is" , "fun", sep = " ", end = "!\n")
+
+#sep  работает как пробел между выводом функции, что-то вроде табуляции, проще рассмотреть на примере. end окончание вывода. \t это как нажатие tab в ворде, добавляет несколько пробелов в выводе
+
+print("foo", "bar")
+print("foo", "bar",sep="")
+print("foo", "bar",sep="->")
+
+print("foo", "bar")
+print("foo", "bar", sep="\t")
+
+
