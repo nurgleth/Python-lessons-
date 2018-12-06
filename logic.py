f02a94665e846a8123b25ac0a12b3b11f9e88caa
Nurgle <axilles90@gmail.com>
2018-12-06 20:06:13 +0300

    Логически операторы, разбор

---
+++ logic.py
@@ -0,0 +1,20 @@
+# оператор "and" это логическое "И"
+# оператор "or" это логическое "ИЛИ"
+# оператор "not" это логическое "НЕ"
+
+a = True
+b = False
+
+print("AND Logick:")
+print("a and a" , a and a)
+print("a and b" , a and b)
+print("b and b" , b and b)
+
+print("\nOR Logick:")
+print("a or a" , a or a)
+print("a or b" , a or b)
+print("b or b" , b or b)
+
+print("\nNOT Logick:")
+print("a=" , a , "\tnot a=", not a)
+print("b=" , b , "\tnot b=", not b)
