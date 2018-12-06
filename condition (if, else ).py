686205fcff3ee1414d43d6d17cd3aa009cf5f3ad
Nurgle <axilles90@gmail.com>
2018-12-06 21:20:21 +0300

    Проверка условий(тернарные операторы) if else

--- condition (if, else ).py
+++ condition (if, else ).py
@@ -5,4 +5,14 @@
 a = 20
 b = 100
 
-print("")
+# пишем пример, тренеруемся
+print("\nVariable a is:" , "supposing true" if (a == 20) else "lie")
+print("Variable a is:" , "supposing true" if (a % 5 == 1) else "odd")
+# пишем пример, тренеруемся
+print("\nVariable b is:" , "supposing true" if (b == 10) else "true")
+print("Variable b is:" , "supposing true" if (b % 100 == 0) else "lie")
+
+# выбираем максимальное значение a or b
+
+max = a if ( a > b) else b
+print("\n Maximum value:" , max)
