c2476384200687c590af8dc9e556c5b5e5190944
Nurgle <axilles90@gmail.com>
2018-12-14 20:04:48 +0300

    Изучаем списки

---
+++ list.py
@@ -0,0 +1,21 @@
+# Списки
+# Списки обозначаются в квадратных скобках и перечисля.тся элементы списка через запятую, например "number = [0,1,2,3,4,5]"
+# Списки могут быть многомерными и содержать строки.
+# Перечисление списков начинается с нуля!
+# Пример вызова  второго данного из списка второй строки "number[1]"
+
+year = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+print("First Month:", year[0])
+print("Second Month:", year[1])
+print("Last Month:", year[11])
+
+scroll = [[1, 2, 3], [4, 5, 6]]
+
+print("\nFirst Left:", scroll[0][0])
+print("Last Right:", scroll[1][2])
+
+print("\nThird Letter in seven  Month:", year[6][3])
+
+
+
