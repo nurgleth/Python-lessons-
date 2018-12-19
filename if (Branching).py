1299a979eb447c631adc14f3eb92ac41e35ddcf6
Nurgle <axilles90@gmail.com>
2018-12-19 22:21:28 +0300

    Ветвление с помощью условного оператора if, elif, else

---
+++ if (Branching).py
@@ -0,0 +1,24 @@
+# Ветвтление с помощью условного оператора
+
+# ключивое слово else должно располагаться строго под словом if
+# синтаксис расмотрим на примерах ниже
+# "elif" второе проверочное выражение
+
+num = int(input("Please enter a Number:"))
+
+if num > 100:
+    print("Enter a Number > 100")
+elif num < 100:
+    num == str
+    print("Enter a Number < 100")
+else:
+    num = 100
+    print("Enter a Number = 100!")
+if num > 10 and num < 8:
+    print("Number = 9")
+if num == 0:
+    print("Number = 0")
+
+
+
+
