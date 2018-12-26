4dd870df4d9965446b4bfd5a1077cb74c44fda16
Nurgle <axilles90@gmail.com>
2018-12-26 19:49:26 +0300

    Разбор цикла While,кое что пробовал

--- While.py
+++ While.py
@@ -7,3 +7,14 @@ while a <= 10:
     while b < 5:
         print("List b:", b)
         b += 1
+
+num = int(input("Please enter a Number = 10:"))
+if num != 10:
+    print("Try once more")
+    int(input("Please enter a Number = 10:"))
+else:
+     num = 10
+     print("Thanks")
+
+
+
